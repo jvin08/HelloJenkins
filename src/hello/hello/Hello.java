5bbfd81d8c4d72bfce9505a8a7ba17be57cfbe59
jvin08 <jvin08@gmail.com>
2020-06-21 10:47:34 -0500

    Hello.class compiled and commited

---
+++ src/hello/hello/Hello.java
@@ -0,0 +1,11 @@
+package hello.hello;
+
+public class Hello {
+
+    public static void main (String[] args){
+        for (int i = 0; i <= 10; i++){
+            System.out.println("Run cycle around park #" + i);
+        }
+        System.out.println("I've made today 10 cycles around the park");
+    }
+}
