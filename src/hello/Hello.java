c5ef8d0aa97df13770df43eceb40c4b6140f578e
jvin08 <jvin08@gmail.com>
2020-06-21 10:37:37 -0500

    Hello.java modified commited'

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
     public static void main (String[] args){
         for (int i = 0; i <= 10; i++){
-            System.out.println("Hello world..." + i);
+            System.out.println("Run cycle around park #" + i);
         }
+        System.out.println("I've made today 10 cycles around the park");
     }
 }
